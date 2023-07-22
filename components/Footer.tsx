af28de6528952666f9398876207db84c1e04ad44
Syahroni Bawafi <syahronibwf@gmail.com>
2023-07-22 08:28:28 +0700

    Footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="text-white">Footer</footer>;
+  return (
+    <footer className="text-slate py-5">
+      <p>
+        Copyright &copy; 2023{" "}
+        <a
+          href="https://github.com/bwafi/syahroni-bawafi"
+          className="text-green hover:border-b border-b-green transition-all ease-button">
+          Syahroni Bawafi.
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
