f055ed76bea28c4c77b577e50b83e748f653de30
Syahroni Bawafi <syahronibwf@gmail.com>
2023-07-24 21:07:51 +0700

    responsive featured section

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -20,45 +20,67 @@ const Featured = () => {
   return (
     <section className="w-full my-10 mx-auto md:px-5 ">
       <Title index="02.">Some Things I’ve Built</Title>
-      <ul className="mt-5 flex flex-col gap-32">
+      <ul className="mt-8 flex flex-col gap-10 md:gap-32">
         {projects.map((project, index: number) => (
           <li
             key={index}
-            className="w-full grid grid-cols-12 items-center group">
-            <div className="relative col-start-6 col-end-13 group-even:col-start-1 group-even:col-end-8 text-right group-even:text-left row-start-1">
+            className="w-full grid grid-cols-12 items-center group rounded">
+            <div className="z-[5] md:z-auto relative col-start-1 md:col-start-7 col-end-13 md:group-even:col-start-1 md:group-even:col-end-7 md:text-right group-even:text-left row-start-1 p-6 md:p-0 navy-shadow md:shadow-none">
               <p className="text-sm font-sfMono text-green">Featured Project</p>
-              <h1 className="text-3xl font-semibold my-3 ">
-                <a href={project.url} className="hover:text-green">
+              <h1 className="small-heading font-semibold my-3 ">
+                <a
+                  href={project.url}
+                  target="_blank"
+                  className="hover:text-green">
                   {project.name}
                 </a>
               </h1>
-              <div className="bg-light-navy rounded p-5 z-[3] relative navy-shadow">
-                <p className="text-light-slate text-lg ">{project.desc}</p>
+              <div className="md:bg-light-navy rounded md:p-5 z-10 relative navy-shadow">
+                <ReactMarkdown
+                  className="text-light-slate text-lg"
+                  components={{
+                    a: ({ node, ...props }) => (
+                      <a
+                        target="_blank"
+                        className="text-green hover:border-b hover:border-b-green"
+                        {...props}
+                      />
+                    ),
+                  }}>
+                  {project.desc}
+                </ReactMarkdown>
               </div>
-              <ul className="flex justify-end group-even:justify-start gap-5 flex-wrap my-5 text-light-slate font-sfMono text-sm z-[3]">
+              <ul className="flex relative md:justify-end group-even:justify-start gap-x-5 flex-wrap my-5 text-light-slate font-sfMono text-[13px] md:text-sm z-[2]">
                 {project.techs.map((tech, index) => (
                   <li key={index}>{tech}</li>
                 ))}
               </ul>
-              <div className="flex text-2xl group-even:justify-start justify-end gap-5 text-light-slate">
-                <a href={project.github} className="hover:text-green">
+              <div className="relative flex text-xl group-even:justify-start justify-start md:justify-end gap-5 z-[2]">
+                <a
+                  href={project.github}
+                  target="_blank"
+                  className="hover:text-green">
                   <FiGithub />
                 </a>
-                <a href={project.url} className="hover:text-green">
+                <a
+                  href={project.url}
+                  target="_blank"
+                  className="hover:text-green">
                   <FiExternalLink />
                 </a>
               </div>
             </div>
-            <div className="group w-full h-full col-start-1 col-end-8 group-even:col-start-6 group-even:col-end-13 z-[1] relative row-start-1 row-end-2">
+            <div className="group/img w-full opacity-25 md:opacity-100 h-full col-start-1 col-end-13 md:col-end-8 md:group-even:col-start-6 md:group-even:col-end-13 z-[2] relative row-start-1 row-end-2">
               <a
                 href={project.url}
-                className="relative img-bg_featured block w-full h-full bg-green mix-blend-screen">
+                target="_blank"
+                className="relative z-[1] md:z-0 img-bg_featured block w-full h-full align-middle bg-green">
                 <Image
                   src={project.image}
                   alt="Quran Portal"
                   width={600}
                   height={300}
-                  className="relative h-full w-full navy-shadow mix-blend-multiply grayscale contrast-90 brightness-90 group-hover:filter-none group-hover:mix-blend-normal"
+                  className="relative h-full w-full object-cover align-middle navy-shadow mix-blend-multiply grayscale contrast-100 brightness-50 md:brightness-90 group-hover/img:filter-none group-hover/img:mix-blend-normal"
                 />
               </a>
             </div>
