e3d516c63eec70ba33d2bcdac0c3ea08ca9222da
Syahroni Bawafi <syahronibwf@gmail.com>
2023-07-30 08:39:39 +0700

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,31 @@ import "../style/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Syahroni Bawafi",
+  title: "Syahroni Bawafi | Portfolio",
   description: "Portfolio Create By NextJs",
+  icons: {
+    icon: [
+      { url: "/image/favicon/favicon.ico" },
+      { sizes: "16x16", url: "/image/favicon/favicon-16x16.png" },
+      { sizes: "32x32", url: "/image/favicon/favicon-32x32.png" },
+      { sizes: "32x32", url: "/image/favicon/favicon-32x32.png" },
+      { sizes: "96x96", url: "/image/favicon/favicon-96x96.png" },
+      { sizes: "192x192", url: "/image/favicon/android-icon-192x192.png" },
+    ],
+    apple: [
+      { url: "/image/favicon/apple-icon.png" },
+      { sizes: "57x57", url: "/image/favicon/apple-icon-57x57.png" },
+      { sizes: "60x60", url: "/image/favicon/apple-icon-60x60.png" },
+      { sizes: "72x72", url: "/image/favicon/apple-icon-72x72.png" },
+      { sizes: "76x76", url: "/image/favicon/apple-icon-76x76.png" },
+      { sizes: "114x114", url: "/image/favicon/apple-icon-114x114.png" },
+      { sizes: "120x120", url: "/image/favicon/apple-icon-120x120.png" },
+      { sizes: "144x144", url: "/image/favicon/apple-icon-144x144.png" },
+      { sizes: "152x152", url: "/image/favicon/apple-icon-152x152.png" },
+      { sizes: "180x180", url: "/image/favicon/apple-icon-180x180.png" },
+    ],
+  },
+  manifest: "/image/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
